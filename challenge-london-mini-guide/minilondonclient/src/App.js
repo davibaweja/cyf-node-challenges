4bf659450de2ab93349db6c0193ab91db09eecff
davibaweja <davibaweja@gmail.com>
2020-11-01 20:54:47 +0000

    v1.1

---
+++ challenge-london-mini-guide/minilondonclient/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import './App.css';
+import Heading from "./Heading"
+
+function App() {
+
+
+  // let Harrow=require("data/Harrow.json");
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Heading/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ challenge-london-mini-guide/minilondonclient/src/CityAndCategory.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+import React from "react"
+
+function CityAndCategory()
+{
+    return (<div>Hello</div>);
+}
+
+export default CityAndCategory;
